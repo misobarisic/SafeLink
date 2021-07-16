356ac7e219aef6095a4c5d45572e1a993310b381
misobarisic <miki.barisic5@gmail.com>
2021-07-16 12:30:12 +0200

    Input classname change

--- components/Input.js
+++ components/Input.js
@@ -2,7 +2,7 @@ export default function Input({placeholder, value, onChangeHandler}) {
     return (
         <input
             type="text"
-            className="shadow-sm  text-center focus:ring-indigo-500 focus:border-indigo-500 block text-sm w-full border-gray-300 rounded-md"
+            className="shadow-sm text-center focus:ring-indigo-500 focus:border-indigo-500 block text-md md:text-xl w-full max-w-2xl border-gray-300 rounded-md"
             placeholder={placeholder}
             value={value}
             onChange={onChangeHandler}
